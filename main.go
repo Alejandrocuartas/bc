1c70ab11941f4918263f4ea402df88728715d3ed
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-06-29 02:28:51 -0500

    s

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/Alejandrocuartas/bc/app"
-	"github.com/Alejandrocuartas/bc/app/db"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-	db.CreateDB()
 	app.RunServer()
 }
